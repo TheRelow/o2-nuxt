15e447349e5de729a33dd208e9a70f0ec796cd5b
TheRelow <dev.antropov@gmail.com>
2021-04-07 13:43:35 +0300

    unresolved

--- components/MapFullscreen.vue
+++ components/MapFullscreen.vue
@@ -14,12 +14,12 @@
         e-mail: <a class="link" href="mailto:ask@o2fit.ru">ask@o2fit.ru</a>
       </div>
       <div class="socials">
-        <img :src="require('@/assets/img/whatsapp.svg')" alt="">
+        <a href="https://wa.me/79616817007?text=%D0%9F%D1%80%D0%B8%D0%B2%D0%B5%D1%82!%20%D0%9A%D0%B0%D0%BA%20%D0%B4%D0%B5%D0%BB%D0%B0" target="_blank"><img :src="require('@/assets/img/whatsapp.svg')" alt=""></a>
         <img :src="require('@/assets/img/telegram.svg')" alt="">
         <img :src="require('@/assets/img/viber.svg')" alt="">
-        <img :src="require('@/assets/img/vk.svg')" alt="">
-        <img :src="require('@/assets/img/facebook.svg')" alt="">
-        <img :src="require('@/assets/img/instagram.svg')" alt="">
+        <a href="https://vk.com/o2fitness" target="_blank"><img :src="require('@/assets/img/vk.svg')" alt=""></a>
+        <a href="https://www.facebook.com/%D0%A4%D0%B8%D1%82%D0%BD%D0%B5%D1%81-%D0%92%D0%BE%D0%BB%D0%B3%D0%BE%D0%B3%D1%80%D0%B0%D0%B4-%D0%9E2-728729987509626" target="_blank"><img :src="require('@/assets/img/facebook.svg')" alt=""></a>
+        <a href="https://www.instagram.com/o2fitness_vlg/?hl=ru" target="_blank"><img :src="require('@/assets/img/instagram.svg')" alt=""></a>
       </div>
       <div class="map-fullscreen__action" @click="toggleInfo"></div>
     </div>
---
+++ components/VideoPopup.vue
@@ -0,0 +1,15 @@
+<template>
+  <FormPopup where="Кнопка под видео"></FormPopup>
+</template>
+
+<script>
+import FormPopup from "@/components/FormPopup";
+export default {
+  name: "VideoPopup",
+  components: { FormPopup },
+}
+</script>
+
+<style scoped>
+
+</style>
