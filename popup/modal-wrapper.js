57485bb99fc40980955a704637c26d480b6cdc46
Egor Antropov <49829668+TheRelow@users.noreply.github.com>
2023-03-22 02:14:22 +0400

    README.md

--- popup/modal-wrapper.js
+++ popup/modal-wrapper.js
@@ -1,36 +1,36 @@
-import './style.scss'
-import Bus from './utils/bus'
-import ModalCmp from './modal'
+import "./style.scss";
+import Bus from "./utils/bus";
+import ModalCmp from "./modal";
 
 export default {
-  name: 'vu-modal-wrapper',
-  data () {
+  name: "vu-modal-wrapper",
+  data() {
     return {
-      modals: []
-    }
+      modals: [],
+    };
   },
   mounted() {
-    if (typeof document !== 'undefined') {
-      document.body.addEventListener('keydown', this.handleTabKey)
+    if (typeof document !== "undefined") {
+      document.body.addEventListener("keydown", this.handleTabKey);
     }
 
-    if (typeof document !== 'undefined') {
-      document.body.addEventListener('keyup', this.handleEscapeKey)
+    if (typeof document !== "undefined") {
+      document.body.addEventListener("keyup", this.handleEscapeKey);
     }
   },
   destroyed() {
-    if (typeof document !== 'undefined') {
-      document.body.removeEventListener('keydown', this.handleTabKey)
+    if (typeof document !== "undefined") {
+      document.body.removeEventListener("keydown", this.handleTabKey);
     }
 
-     if (typeof document !== 'undefined') {
-      document.body.removeEventListener('keyup', this.handleEscapeKey)
+    if (typeof document !== "undefined") {
+      document.body.removeEventListener("keyup", this.handleEscapeKey);
     }
   },
   created() {
-    Bus.$on('new', options => {
+    Bus.$on("new", (options) => {
       const defaults = {
-        title: '',
+        title: "",
         dismissable: true,
         center: false,
         fullscreen: false,
@@ -39,121 +39,119 @@ export default {
         isLeft: false,
         isRight: false,
         isScroll: false,
-        className: '',
-        size: 'md',
+        className: "",
+        size: "md",
         escapable: false,
         bodyPadding: true,
         onClose() {},
-        onDismiss() {}
+        onDismiss() {},
       };
 
-      let instance = {}
-      let rendered
-      if(options.component.template) {
-        rendered = false
+      let instance = {};
+      let rendered;
+      if (options.component.template) {
+        rendered = false;
       } else {
         try {
-          rendered = options.component.render.call(this, this.$createElement)
+          rendered = options.component.render.call(this, this.$createElement);
         } catch {
-          console.log('какая-то непонятная ошибка с отсутвием компонента в компоненте. Можно легко пофиксить, но это будет какое-то странное решение')
-          console.log('при том, если обработать эту ошибку через try, catch - то проблемм нет. Пока решение не нашел')
+          console.log(
+            "какая-то непонятная ошибка с отсутвием компонента в компоненте. Можно легко пофиксить, но это будет какое-то странное решение"
+          );
+          console.log(
+            "при том, если обработать эту ошибку через try, catch - то проблемм нет. Пока решение не нашел"
+          );
         }
       }
-        
-      if(rendered && rendered.componentOptions && rendered.componentOptions.Ctor.extendOptions.componentName === 'vu-modal') {
-        const propsData = rendered.componentOptions.propsData
+
+      if (
+        rendered &&
+        rendered.componentOptions &&
+        rendered.componentOptions.Ctor.extendOptions.componentName ===
+          "vu-modal"
+      ) {
+        const propsData = rendered.componentOptions.propsData;
         instance = {
           isVmodal: true,
-          options: Object.assign(defaults, propsData, options)
-        }
+          options: Object.assign(defaults, propsData, options),
+        };
       } else {
         instance = {
           isVmodal: false,
-          options: Object.assign(defaults, options)
-        }
+          options: Object.assign(defaults, options),
+        };
       }
-      rendered = null
+      rendered = null;
 
       this.modals.push(instance);
 
-      Bus.$emit('opened', {
+      Bus.$emit("opened", {
         index: this.$last,
-        instance
+        instance,
       });
 
-      this.body && this.body.classList.add('modals-open');
+      this.body && this.body.classList.add("modals-open");
     });
 
-    Bus.$on('close', data => {
+    Bus.$on("close", (data) => {
       let index = null;
 
-      if (data && data.$index)
-        index = data.$index;
+      if (data && data.$index) index = data.$index;
 
-      if (index === null)
-        index = this.$last;
+      if (index === null) index = this.$last;
 
       this.close(data, index);
     });
 
-    Bus.$on('dismiss', index => {
-      if (index === null)
-        index = this.$last;
+    Bus.$on("dismiss", (index) => {
+      if (index === null) index = this.$last;
 
       this.dismiss(index);
     });
   },
   methods: {
     splice(index = null) {
-      if (index === -1)
-        return;
+      if (index === -1) return;
 
-      if (!this.modals.length)
-        return;
+      if (!this.modals.length) return;
 
-      if (index === null)
-        this.modals.pop();
-      else
-        this.modals.splice(index, 1);
+      if (index === null) this.modals.pop();
+      else this.modals.splice(index, 1);
 
       if (!this.modals.length) {
-        this.body && this.body.classList.remove('modals-open');
-        Bus.$emit('destroyed');
+        this.body && this.body.classList.remove("modals-open");
+        Bus.$emit("destroyed");
       }
     },
 
     doClose(index) {
-      if (!this.modals.length)
-        return;
+      if (!this.modals.length) return;
 
-      if (!this.modals[index])
-        return;
+      if (!this.modals[index]) return;
 
       this.splice(index);
     },
 
     close(data = null, index = null) {
-      if (this.modals.length === 0)
-        return;
+      if (this.modals.length === 0) return;
 
       let localIndex = index;
 
-      if (index && typeof index === 'function') {
+      if (index && typeof index === "function") {
         localIndex = index(data, this.modals);
       }
 
-      if (typeof localIndex !== 'number')
-        localIndex = this.$last;
+      if (typeof localIndex !== "number") localIndex = this.$last;
 
-      Bus.$emit('closed', {
+      Bus.$emit("closed", {
         index: localIndex,
         instance: this.modals[index],
-        data
+        data,
       });
 
       if (localIndex !== false && this.modals[localIndex]) {
-        if(this.modals[localIndex].options.onClose(data) === false) {
-          return
+        if (this.modals[localIndex].options.onClose(data) === false) {
+          return;
         }
       }
       this.doClose(localIndex);
@@ -162,37 +160,32 @@ export default {
     dismiss(index = null) {
       let localIndex = index;
 
-      if (index && typeof index === 'function')
-        localIndex = index(this.$last);
+      if (index && typeof index === "function") localIndex = index(this.$last);
 
-      if (typeof localIndex !== 'number')
-        localIndex = this.$last;
+      if (typeof localIndex !== "number") localIndex = this.$last;
 
-      if (this.modals[localIndex].options.onDismiss() === false)
-        return;
+      if (this.modals[localIndex].options.onDismiss() === false) return;
 
-      Bus.$emit('dismissed', {
+      Bus.$emit("dismissed", {
         index: localIndex,
-        instance: this.modals[localIndex]
+        instance: this.modals[localIndex],
       });
 
       this.doClose(localIndex);
     },
 
-     handleTabKey(e) {
-       if (e.keyCode === 9 && this.modals.length) {
-         e.preventDefault()
-       }
+    handleTabKey(e) {
+      if (e.keyCode === 9 && this.modals.length) {
+        e.preventDefault();
+      }
     },
 
     handleEscapeKey(e) {
       if (e.keyCode === 27 && this.modals.length) {
-        if (!this.modals.length)
-          return;
-        if (this.current.options.escapable)
-          this.dismiss();
+        if (!this.modals.length) return;
+        if (this.current.options.escapable) this.dismiss();
       }
-    }
+    },
   },
   computed: {
     current() {
@@ -202,56 +195,81 @@ export default {
       return this.modals.length - 1;
     },
     body() {
-      if (typeof document !== 'undefined') {
-        return document.querySelector('body');
+      if (typeof document !== "undefined") {
+        return document.querySelector("body");
       }
     },
     wrapperStyle() {
       return {
-        'z-index': 5000 + this.$last + 1
-      }
-    }
+        "z-index": 5000 + this.$last + 1,
+      };
+    },
   },
   render(h) {
-    if(!this.modals.length) {
-        return null
-    };
+    if (!this.modals.length) {
+      return null;
+    }
 
     let modals = this.modals.map((modal, index) => {
-      let modalComponent 
+      let modalComponent;
 
-      if(modal.isVmodal) {
+      if (modal.isVmodal) {
         modalComponent = h(modal.options.component, {
-          props: Object.assign({}, {vModal: Object.assign(modal.options, { disabled: index != this.$last })}, modal.options.props)
-       })
+          props: Object.assign(
+            {},
+            {
+              vModal: Object.assign(modal.options, {
+                disabled: index != this.$last,
+              }),
+            },
+            modal.options.props
+          ),
+        });
       } else {
-        modalComponent = h(ModalCmp, {
-          props: Object.assign(modal.options, { disabled: index != this.$last })
-        }, [
-          h(modal.options.component, { 
-            props: modal.options.props
-          })
-        ])
+        modalComponent = h(
+          ModalCmp,
+          {
+            props: Object.assign(modal.options, {
+              disabled: index != this.$last,
+            }),
+          },
+          [
+            h(modal.options.component, {
+              props: modal.options.props,
+            }),
+          ]
+        );
       }
-      return h('div', {
-        class: ['vu-modal__mask', {'vu-modal__mask--disabled': index != this.$last }],
-        on: {
-          mousedown: (e) => {
-            if (e.path[0].className == 'vu-modal__mask') {
-              modal.options.dismissable && this.dismiss()
-            }
-          }
+      return h(
+        "div",
+        {
+          class: [
+            "vu-modal__mask",
+            { "vu-modal__mask--disabled": index !== this.$last },
+          ],
+          on: {
+            mousedown: (e) => {
+              console.log("e", e);
+              if (
+                e?.srcElement &&
+                e.srcElement[0]?.className === "vu-modal__mask"
+              ) {
+                modal.options.dismissable && this.dismiss();
+              }
+            },
+          },
+          key: index,
         },
-        key: index
-      }, [ 
-        modalComponent 
-      ])
-    })
+        [modalComponent]
+      );
+    });
 
-    return h('div', {
-      class: 'vu-modal__wrapper',
-    }, [ modals ]) 
-  }
+    return h(
+      "div",
+      {
+        class: "vu-modal__wrapper",
+      },
+      [modals]
+    );
+  },
 };
-
-
